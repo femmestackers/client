c5ec80cf11362e1009a987c77bc0b8d17cca7c44
SumayaAlam19 <sumayaalam123@gmail.com>
2020-07-23 11:07:43 +1000

    more css and forms of different pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,6 @@ const Home = () => {
       <>
       <div className="first-container-home">
           <div className="second-container-home" alt="Picture of home">
-              <div className="navbar">
-                  <div className="navelement1">
-                      <p className="navtitle">SMOOTHIVERSE</p>
-                  </div>
-                  <div className="navelement2">
-                      <p className="navtext">Home</p>
-                      <p className="navtext">Add Smoothie</p>
-                      <p className="navtext">View All</p>
-                      <p className="navtext">Sign Up</p>
-                      <p className="navtext">Log In</p>
-                  </div>
-              </div>
               <div>
                   <p className="welcometext">
                   Smoothies have gained fame over the past years for a host of health benefits with the added bonus of being tasty!  And with the internet allowing the exchange of information at wildfire-pace, this app makes use of people’s passion for smoothies! It allows the users to view smoothie recipes from their favourite categories and post comments on what they think of them. It also allows users to post their recipes, so that others can try
@@ -25,9 +13,6 @@ const Home = () => {
                   </p>
               </div>
           </div>
-          <p className="container-footer">
-          &copy; Copyright Smoothiverse 2020
-          </p>
       </div>
       </>
     )
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import './Styles.css'
+
+const Nav = () =>{
+    return (
+        <div className="second-container-home" alt="Picture of home">
+        <div className="navbar">
+                <div className="navelement1">
+                    <p className="navtitle">SMOOTHIVERSE</p>
+                </div>
+                <div className="navelement2">
+                    <Link className="navtext" to="/" >Home</Link>
+                    <Link className="navtext" to="signup" >SignUp</Link>
+                    <Link className="navtext" to="login" >LogIn</Link>
+                    <Link className="navtext" to="smoothies" >ViewAll</Link>
+                    <Link className="navtext" to="newsmoothie" >AddSmoothie</Link>
+                    <Link className="navtext" to="/" >LogOut</Link>
+                </div>
+        </div>
+        </div>
+
+        
+          )
+    }
+    export default Nav
