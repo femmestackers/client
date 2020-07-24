0b8d300aa766545992c7ee746e2ee1e16813d09a
ash-krish <aishu1188@gmail.com>
2020-07-24 12:22:11 +1000

    connecting data from server
    Co-Authored-By: Sumaya <sumayaalam123@gmail.com>
    Co-Authored-By: Aubrey <aubree.gamboa@gmail.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useGlobalState} from '../config/store'
+import {loginUser, setLoggedInUser} from '../services/authServices'
 import './Styles.css'
 
-const Login = () =>{
+
+const Login = ({history}) => {
+    const initialFormState = {
+        email: "",
+        password: ""
+    } 
+    const [userDetails,setUserDetails] = useState(initialFormState)
+    const [errorMessage, setErrorMessage] = useState(null)
+    
+    
+
+    function handleChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setUserDetails({
+            ...userDetails,
+            [name]: value
+        })
+    }
+    /*function validateForm() {
+        return email.length > 5 && password.length > 5;
+      }
+      */
+    
+    function handleSubmit(event) {
+        event.preventDefault()
+        // Attempt login on server
+        loginUser(userDetails).then(() => {
+            setLoggedInUser(userDetails.username)
+           setErrorMessage(null)
+            history.push("/")
+
+        }
+        )
+        .catch((error) => {
+            console.log("error", error)
+            if (error.response && error.response.status === 401)
+                setErrorMessage("Authentication failed. Please check your username and password.")
+            else   
+                setErrorMessage("There may be a problem with the server. Please try again after a few moments.")
+        })		
+    }
+
+   
+
+
 return (
     <div className="first-container-login">
-        <form>
+        <form onSubmit={handleSubmit}>
+            {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
             <div>
-                <label>Email:<input type="text" name="email" /></label>
-            </div><br/>
+                <label>Username<input type="text" name="username" onChange={handleChange}/></label>
+            </div>
+            <br/>
             <div>
-                <label>Password:<input type="password" name="password" /></label>
+                <label>Password:<input type="password" name="password" onChange={handleChange}/></label>
             </div><br/>
-                <input type="submit" value="LogIn" />
+                <input type="submit" name="LogIn"/>
     </form>
     </div>
       )
 }
+
 export default Login
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,29 +1,55 @@
-import React from 'react'
 import './Styles.css'
+import React, {useState} from 'react'
+import {useGlobalState} from '../config/store'
 
-const Register = () =>{
-    console.log("register");
+const Register = ({history}) => {
+    const initialFormState = {
+        username: "",
+        email: "",
+        password: "",
+        
+    } 
+    const [userDetails,setUserDetails] = useState(initialFormState)
+    const dispatch = useGlobalState()
+
+    function handleChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setUserDetails({
+            ...userDetails,
+            [name]: value
+        })
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        dispatch({
+            type: "setLoggedInUser",
+            data: userDetails.username
+        })
+        history.push("/")
+    }
+
 return (
   <div className="first-container-signup">
-      <form>
+      <form onSubmit={handleSubmit}>
           <div>
               <label>Username</label>
-              <input type="text" name="username" placeholder="Enter a username"></input>
+              <input type="text" name="username" placeholder="Enter a username"  onChange={handleChange}></input>
           </div>
           <br/>
           <div>
               <label>Email</label>
-              <input type="text" name="email" placeholder="Enter an email"></input>
+              <input type="text" name="email" placeholder="Enter an email"  onChange={handleChange}></input>
           </div>
           <br/>
           <div>
               <label>Password</label>
-              <input type="password" name="password" placeholder="Enter a password"></input>
+              <input type="password" name="password" placeholder="Enter a password"  onChange={handleChange}></input>
           </div>
           <br/>
           <div>
               <label>Confirm Password</label>
-              <input type="password" name="confirm-password" placeholder="Confirm the password"></input>
+              <input type="password" name="confirm-password" placeholder="Confirm the password"  onChange={handleChange}></input>
           </div>
           <br/>
           <input type="submit" value="SignUp"></input>
@@ -31,4 +57,6 @@ return (
   </div>
 )
 }
+
+
 export default Register
