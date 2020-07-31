a8a883fa91401c3c367ea43cd96320a3c02341ac
augam8 <aubree.gamboa@gmail.com>
2020-07-31 16:25:59 +1000

    changes for deployment

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // Create an axios instance
 export default axios.create({
-baseURL: "http://localhost:3020",
+
+baseURL: "https://smoothiverse-server.herokuapp.com/",
 timeout: 30000,
 withCredentials: true
 })
